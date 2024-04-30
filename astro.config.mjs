370b868abe58fa63272469285ece76c58fed3b94
Kohki Shiga <saggitarie@gmail.com>
2024-04-30 13:04:22 +0900

    feat: enable https certificate for localhost

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,30 @@
 import { defineConfig } from 'astro/config';
+import storyblok from '@storyblok/astro';
+import tailwind from '@astrojs/tailwind'
+import { loadEnv } from 'vite';
+import mkcert from'vite-plugin-mkcert'
+
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [
+		storyblok({
+				accessToken: env.STORYBLOK_TOKEN,
+				components: {
+					// Add your components here
+					blogPost: 'storyblok/BlogPost',
+				},
+				apiOptions: {
+					// Choose your Storyblok space region
+				region: 'ap', // optional,  or 'eu' (default)
+			},
+		}),
+		tailwind(),
+	],
+	vite: {
+		plugins: [ mkcert({
+			savePath: "./.cert"
+		}) ]
+	}
+});

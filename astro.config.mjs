31a27b7cd675248107fd0badd99e4a871dd45c5c
Kohki Shiga <saggitarie@gmail.com>
2024-04-30 13:39:34 +0900

    feat: enable tailwind styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,31 @@
 import { defineConfig } from 'astro/config';
 import storyblok from '@storyblok/astro';
-import tailwind from '@astrojs/tailwind'
 import { loadEnv } from 'vite';
-import mkcert from'vite-plugin-mkcert'
-
+import mkcert from 'vite-plugin-mkcert';
+import tailwind from "@astrojs/tailwind";
 const env = loadEnv("", process.cwd(), 'STORYBLOK');
+
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		storyblok({
-				accessToken: env.STORYBLOK_TOKEN,
-				components: {
-					// Add your components here
-					page: 'storyblok/Page',
-					feature: 'storyblok/Feature',
-					grid: 'storyblok/Grid',
-					teaser: 'storyblok/Teaser',
-				},
-				apiOptions: {
-					// Choose your Storyblok space region
-					region: 'ap', // optional,  or 'eu' (default)
-				},
-		}),
-		tailwind(),
-	],
-	vite: {
-		plugins: [ mkcert({
-			savePath: "./.cert"
-		}) ]
-	}
+  integrations: [storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    components: {
+      // Add your components here
+      page: 'storyblok/Page',
+      feature: 'storyblok/Feature',
+      grid: 'storyblok/Grid',
+      teaser: 'storyblok/Teaser'
+    },
+    apiOptions: {
+      // Choose your Storyblok space region
+      region: 'ap' // optional,  or 'eu' (default)
+    }
+  })
+  , tailwind()],
+  vite: {
+    plugins: [mkcert({
+      savePath: "./.cert"
+    })]
+  }
 });

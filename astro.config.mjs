4b78e8a053da7d48ef05c292760a5a3c18384c6d
Kohki Shiga <saggitarie@gmail.com>
2024-05-19 13:24:30 +0900

    fix: lint syntax

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,28 +6,32 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 const env = loadEnv("", process.cwd(), "STORYBLOK");
 
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [storyblok({
-    accessToken: env.STORYBLOK_TOKEN,
-    components: {
-      // Add your components here
-      page: "storyblok/Page",
-      feature: "storyblok/Feature",
-      grid: "storyblok/Grid",
-      teaser: "storyblok/Teaser"
-    },
-    apiOptions: {
-      // Choose your Storyblok space region
-      region: "ap" // optional,  or 'eu' (default)
-    }
-  }), tailwind()],
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        // Add your components here
+        page: "storyblok/Page",
+        feature: "storyblok/Feature",
+        grid: "storyblok/Grid",
+        teaser: "storyblok/Teaser",
+      },
+      apiOptions: {
+        // Choose your Storyblok space region
+        region: "ap", // optional,  or 'eu' (default)
+      },
+    }),
+    tailwind(),
+  ],
   vite: {
-    plugins: [mkcert({
-      savePath: "./.cert"
-    })]
+    plugins: [
+      mkcert({
+        savePath: "./.cert",
+      }),
+    ],
   },
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
 });
